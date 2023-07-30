a640c21f55957bb422b30dcad9357dd68844a0e5
Eyal Yovel <eyalyovel@gmail.com>
2023-07-30 11:50:47 +0100

    Update utils.js

--- test/helpers/utils.js
+++ test/helpers/utils.js
@@ -1,15 +1,15 @@
 
-// export async function restartApp() {
+export async function restartApp() {
 
-// 	if (driver.isAndroid){
-// 		let packageName = await driver.getCurrentPackage();
-// 		console.log("*** package name is " + packageName);
-// 		await driver.terminateApp(packageName);
-// 		await driver.activateApp(packageName);
-// 	}
+	if (driver.isAndroid){
+		let packageName = await driver.getCurrentPackage();
+		console.log("*** package name is " + packageName);
+		await driver.terminateApp(packageName);
+		await driver.activateApp(packageName);
+	}
 
-// };
+};
 
-// export function locatorStrategy(selector) {
-// 	return driver.isIOS ? `id=${selector}` : `//*[@content-desc="${selector}"]`;
-// };
+export function locatorStrategy(selector) {
+	return driver.isIOS ? `id=${selector}` : `//*[@content-desc="${selector}"]`;
+};
